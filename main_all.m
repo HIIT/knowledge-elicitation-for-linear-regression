7848dcc01160b6b9fd689bd9d587209473b6773a
PedramDaee <pedram.daee@gmail.com>
2016-08-09 17:56:22 +0300

    new model for feedback on relevance of features (MODE=2)

--- main_all.m
+++ main_all.m
@@ -2,31 +2,35 @@ close all
 clear all
 
 %% Parameters and Simulator setup 
-mode                     = 1; % 0: Gaussian prior, 1: spike and slab prior
+MODE                     = 2; 
+% MODE specifies the  type of feedback and the model that we are using
+%           0: Feedback on weight values. Model: Gaussian prior 
+%           1: Feedback on weight values. Model: spike and slab prior
+%           2: Feedback on relevance of features. Model: spike and slab prior
 %data parameters
 num_features             = 100; %[start,step,max] This can be a set of values (e.g. 1:100) or just one value (e.g. 100)
-num_trainingdata         = 5:10:500; %[start,step,max] This can be a set of values (e.g. 1:10:500) or just one value (e.g. 5)
+num_trainingdata         = 5:20:400; %[start,step,max] This can be a set of values (e.g. 1:10:500) or just one value (e.g. 5)
 max_num_nonzero_features = 10; % maximum number of features that are nonzero --- AKA sparsity measure
 
 
 %Algorithm parameters
 num_iterations = 50; %total number of user feedback
-num_runs       = 50;
+num_runs       = 50; %total number of runs (necessary for averaging results) 
 
 %model parameters
-model_params   = struct('Nu_y',0.5, 'Nu_theta', 1, 'Nu_user', 0.1);
+model_params   = struct('Nu_y',0.5, 'Nu_theta', 1, 'Nu_user', 0.1, 'P_user', 0.9);
 normalization_method = 1; %normalization method for generating the data (Xs)
 sparse_options = struct('damp',0.5, 'damp_decay',1, 'robust_updates',2, 'verbosity',0, 'max_iter',100, 'threshold',1e-5, 'min_site_prec',1e-6);
-sparse_params  = struct('sigma2',model_params.Nu_y^2, 'tau2', model_params.Nu_theta^2 ,'eta2',model_params.Nu_user^2,'p_u',0.9);
+sparse_params  = struct('sigma2',model_params.Nu_y^2, 'tau2', model_params.Nu_theta^2 ,'eta2',model_params.Nu_user^2,'p_u', model_params.P_user);
 %% METHOD LIST
 % Set the desirable methods to 'True' and others to 'False'. only the 'True' methods will be considered in the simulation
 METHODS_ALL = {
      'True',  'Max(90% UCB,90% LCB)'; 
      'True',  'Uniformly random';
      'False', 'random on the relevelant features';
-     'False', 'max variance';
+     'True', 'max variance';
      'False', 'Bayes experiment design';
-     'True',  'Expected information gain';
+     'False',  'Expected information gain';
      'False', 'Bayes experiment design (tr.ref)';
      'True',  'Expected information gain (post_pred)'
      };
@@ -37,7 +41,7 @@ for m = 1:size(METHODS_ALL,1)
     end
 end
 num_methods = size(Method_list,2); %number of decision making methods that we want to consider
-%% Main algorithm
+%% Main simulation
 
 Loss_1 = zeros(num_methods, num_iterations, num_runs, size(num_features,2),size(num_trainingdata,2));
 Loss_2 = zeros(num_methods, num_iterations, num_runs, size(num_features,2),size(num_trainingdata,2));
@@ -56,7 +60,7 @@ for n_f = 1:size(num_features,2);
             % non-zero elements of theta_star are generated based on the model parameters
             theta_star = model_params.Nu_theta*randn( num_nonzero_features, 1); % We are using randn to generate theta start
             theta_star = [theta_star; zeros(num_features(n_f)-num_nonzero_features,1)]; % make it sparse
-
+            z_star = theta_star ~= 0; % the true value for the latent variable Z in spike and slab model
             %generate new data for each run (because the results is sensitive to the covariate values)
             X_all   = generate_data(num_data,num_features(n_f), normalization_method);
             X_train = X_all(1:num_trainingdata(n_t),:)'; % select a subset of data as training data
@@ -66,10 +70,11 @@ for n_f = 1:size(num_features,2);
             Y_test  = normrnd(X_test'*theta_star, model_params.Nu_y); % calculate drug responses of the test data
             for method_num = 1:num_methods
                 method_name = Method_list(method_num);
-                Theta_user = []; %user feedback which is a (N_user * 2) array containing [feedback value, feature_number].
+                %Feedback = values (1st column) and indices (2nd column) of user feedback
+                Feedback = []; 
                 sparse_options.si = []; % carry prior site terms between interactions
                 for it = 1:num_iterations %number of user feedback
-                    posterior = calculate_posterior(X_train, Y_train, Theta_user, model_params, mode, sparse_params, sparse_options);
+                    posterior = calculate_posterior(X_train, Y_train, Feedback, model_params, MODE, sparse_params, sparse_options);
                     sparse_options.si = posterior.si;
                     Posterior_mean = posterior.mean;
                     %% calculate different loss functions
@@ -85,10 +90,11 @@ for n_f = 1:size(num_features,2);
                     log_post_pred = -log(sqrt(2*pi*post_pred_var)) - ((X_train'*Posterior_mean - Y_train).^2)./(2*post_pred_var);
                     Loss_4(method_num, it, run, n_f ,n_t) = mean(log_post_pred);
                     %% make decisions based on a decision policy
-                    feature_index = decision_policy(posterior, method_name, num_nonzero_features, X_train, Y_train, Theta_user, model_params, mode, sparse_params, sparse_options);
+                    feature_index = decision_policy(posterior, method_name, num_nonzero_features, X_train, Y_train, Feedback, model_params, MODE, sparse_params, sparse_options);
                     decisions(method_num, it, run, n_f ,n_t) = feature_index;
                     %simulate user feedback
-                    Theta_user = [Theta_user; normrnd(theta_star(feature_index),model_params.Nu_user), feature_index];
+                    new_fb_value = user_feedback(feature_index, theta_star, z_star, MODE, model_params);
+                    Feedback = [Feedback; new_fb_value , feature_index];
                 end
             end
         end
@@ -96,5 +102,5 @@ for n_f = 1:size(num_features,2);
 end
 
 %% averaging and plotting
-save('results_all', 'Loss_1', 'Loss_2', 'Loss_3', 'Loss_4', 'decisions', 'num_nonzero_features', 'Method_list', 'num_features','num_trainingdata')
+save('results_all', 'Loss_1', 'Loss_2', 'Loss_3', 'Loss_4', 'decisions', 'num_nonzero_features', 'Method_list', 'num_features','num_trainingdata', 'MODE')
 evaluate_results_all
