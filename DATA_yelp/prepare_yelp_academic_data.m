167580e8c48623c13f1c8b067240d9bb1754e23d
MartaSoare <martasoare88@gmail.com>
2016-09-15 01:41:03 +0300

    Add files via upload

---
+++ DATA_yelp/prepare_yelp_academic_data.m
@@ -0,0 +1,15 @@
+load('yelp_academic_data_raw.mat');
+%it gives us X_all_raw, Y_all, keywords_all
+%% Filter the data to only contain features with more than thershold appearance
+appearance_threshold = 50;
+
+num_data     = size(X_all_raw,1);
+num_orig_dim = size(X_all_raw,2);
+
+appearance_per_feature = sum(X_all_raw ~= 0,1);
+filtered_features = appearance_per_feature> appearance_threshold;
+X_all = X_all_raw(:,filtered_features);
+keywords = keywords_all(filtered_features);
+
+save('yelp_academic_data','X_all','Y_all','keywords');
+disp([' number of data ', num2str(num_data), ', num original dimensions ', num2str(num_orig_dim), ', number of new dimensions ', num2str(size(X_all,2)) ]);
